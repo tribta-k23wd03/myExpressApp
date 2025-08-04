6c0249299b7a4dcc3f68871b2a213ee905a8f235
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-04 21:00:50 +0700

    update

---
+++ mongo-express-app/src/controllers/image.controller.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import { AuthRequest } from "../middlewares/auth.middlewares";
+import { Image } from "../models/Image";
+
+export const uploadImage = async (res: Response, req: AuthRequest) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "Missing image file" });
+  }
+
+  const { description, visibility } = req.body;
+
+  const newImage = new Image({
+    user: req.user?.id,
+    fileName: req.file.filename,
+    description,
+    visibility: visibility === "private" ? "private" : "public",
+    status: "pending",
+  });
+
+  const saved = await newImage.save();
+
+  res.status(201).json({
+    message: "image uploaded. Awaiting admin approval!",
+    image: saved,
+  });
+};
